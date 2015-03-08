a4aeebbf1464b89bad322b8dfa555a99936e6c08
Aman King <amanking@gmail.com>
2015-03-08 11:51:29 +0530

    Removed logic of auto-requiring files from spec_helper and requiring in corresponding spec itself

--- spec/breadcrumbs/breadcrumbs_addon_spec.rb
+++ spec/breadcrumbs/breadcrumbs_addon_spec.rb
@@ -1,4 +1,5 @@
 require_relative '../spec_helper'
+require_relative '../../breadcrumbs/breadcrumbs_addon'
 
 describe BreadcrumbsAddon do
   before :each do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,2 @@
 require 'rspec'
 require 'ruhoh'
-
-Dir["**/*.rb"].each { |file| require File.expand_path(file, ".") unless file =~ /^spec|vendor|\./ }
